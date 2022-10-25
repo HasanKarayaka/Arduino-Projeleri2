ef8795e6167502155e57b2d97efeb213d965aa31
Hasan <karayaka34@outlook.com>
2022-10-25 22:09:11 +0300

    LDR kullanımmı

---
+++ LDR/arduino/LDR.ino
@@ -0,0 +1,19 @@
+void setup() {
+  pinMode(3,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+int isik = analogRead(A0);
+delay(50);
+
+if(isik > 900)
+{
+ digitalWrite(3,LOW);
+}
+if(isik < 750)
+{
+ digitalWrite(3,HIGH);
+}
+
+}
